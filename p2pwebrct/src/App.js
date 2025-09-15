0c6d348e0168cf2ce9e6ff72f044df9f3cb43bd4
PepsaCola <162103453+PepsaCola@users.noreply.github.com>
2025-09-15 08:07:47 +0200

    working version
    Comleted version of video chat

---
+++ p2pwebrct/src/App.js
@@ -0,0 +1,18 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Room from './pages/Room';
+import Main from './pages/Main';
+import NotFound404 from './pages/NotFound404';
+
+function App() {
+  return (
+
+        <Routes>
+          <Route exact path='/room/:id' element={<Room/>}/>
+          <Route exact path='/' element={<Main/>}/>
+          <Route element={<NotFound404/>}/>
+        </Routes>
+
+  );
+}
+
+export default App;
